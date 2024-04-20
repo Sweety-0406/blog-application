241abbc3775236cda2db781163976d9fa2a27a6b
Sweety Nandi <sweetynandi06062002@gmail.com>
2024-04-20 10:55:54 +0530

    blog page  modification

--- app/blogProperties/page.tsx
+++ app/blogProperties/page.tsx
@@ -1,46 +1,57 @@
-import ClientOnly from "../components/ClientOnly";
-import EmptyState from "../components/EmptyState";
 import getCurrentUser from "../action/getCurrentUser";
-import PropertiesClient from "./PropertiesClient";
 import { getListings } from "../action/getListings";
-
-const PropertiesPage =async () => {
-    const currentUser = await getCurrentUser();
-    if(!currentUser){
-        return(
-            <ClientOnly>
-                <EmptyState
-                  title="Unauthorized"
-                  subtitle="Please login!"
-                 />
-            </ClientOnly>
-        )
-            
-    }
-    const listings = await getListings({
-        userId : currentUser.id
-    });
-
-    if(listings.length === 0){
+import EmptyState from "../components/EmptyState";
+import PropertiesClient from "./PropertiesClient";
+import prisma from "@/app/libs/prismadb";
+//@ts-ignore
+export default function PropertiesPage({ currentUser, listings }) {
+    if (!currentUser) {
         return (
-            <ClientOnly>
-                <EmptyState 
-                  title="No properties found."
-                  subtitle="Looks like you have no properties."
-                />
-            </ClientOnly>
-        )
+            <EmptyState
+                title="Unauthorized"
+                subtitle="Please login!"
+            />
+        );
     }
 
-    return(
-        <ClientOnly>
-            <PropertiesClient
-              currentUser={currentUser}
-              listings={listings} 
+    if (listings.length === 0) {
+        return (
+            <EmptyState
+                title="No properties found."
+                subtitle="Looks like you have no properties."
             />
-        </ClientOnly>
-    )
+        );
+    }
 
+    return (
+        <PropertiesClient
+            currentUser={currentUser}
+            listings={listings}
+        />
+    );
 }
 
-export default PropertiesPage;
+export async function getServerSideProps() {
+    try {
+        // Fetch currentUser and listings here
+        const currentUser = await getCurrentUser();
+        const listings = await getListings({
+            userId: currentUser?.id
+        });
+
+        return {
+            props: {
+                currentUser,
+                listings
+            }
+        };
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return {
+            props: {
+                currentUser: null,
+                listings: []
+            }
+        };
+    }
+}
