85d7c539fe1eb088f25dc86a0fe90c1d666f34a1
Sweety Nandi <sweetynandi06062002@gmail.com>
2024-06-21 23:18:20 +0530

    middleware modify

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,9 @@ export {default} from 'next-auth/middleware';
 
 export const config ={
     matcher:[
-        "/trips",
-        "/reservations",
-        "/properties",
-        "/favorites"
+        "/blogProperties",
+        "/favorites",
+        "/about",
+        "/contact"
     ]
 }
